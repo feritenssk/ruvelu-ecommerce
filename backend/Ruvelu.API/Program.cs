37a00da2d70ec6ed307b4bce170874ca0acd0a8e
Ferit Enes Işık <feritenesimm@gmail.com>
2026-06-29 05:34:12 +0300

    feat: configure DI and database connection in Program.cs

--- backend/Ruvelu.API/Program.cs
+++ backend/Ruvelu.API/Program.cs
@@ -1,41 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using Ruvelu.Core.Interfaces;
+using Ruvelu.Data;
+using Ruvelu.Data.Repositories;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+var builder = webApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
+builder.Services.AddDbContext<RuveluDbContext>(options =>
+ options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
-app.UseHttpsRedirection();
+ var app = builder.Build();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+ if (app.Environment.IsDevelopment())
+ {
+     app.MapOpenApi();
+ }
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+ app.UseHttpsRedirection();
+ app.UseAuthorization();
+ app.MapControllers();
 
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+ app.Run();
