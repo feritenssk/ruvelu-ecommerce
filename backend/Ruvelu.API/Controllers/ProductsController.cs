24230c188646f736a5756a2ec86d95465824fc62
Ferit Enes Işık <feritenesimm@gmail.com>
2026-06-30 06:08:18 +0300

    feat: add GetAll, Create and GetById endpoints to ProductsController

---
+++ backend/Ruvelu.API/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Ruvelu.Core.Entities;
+using Ruvelu.Core.Interfaces;
+
+namespace Ruvelu.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+{
+    private readonly IProductRepository _productRepository;
+    
+    public ProductsController(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult>GetAll()
+    {
+        var products = await _productRepository.GetAllAsync();
+        return Ok(products);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create (Product product)
+        {
+            await _productRepository.AddAsync(product);
+            return CreatedAtAction(nameof(GetById), new {id = product.Id},product);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+}
+
+}
+
